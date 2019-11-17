71e3171e13ada9c680ce89ad1a078ccac5a23841
PrzemekPolak <przemekpolak138@gmail.com>
2019-11-17 21:30:37 +0100

    zadania

---
+++ 03-WebTechnologies/js/buttonclick.js
@@ -0,0 +1,8 @@
+var x = 0
+
+function funk() {
+    x ++ 
+    console.log('You clicked the button '+ x +' times')
+}
+
+
---
+++ 03-WebTechnologies/js/date.js
@@ -0,0 +1,8 @@
+var now = new Date()
+
+var dzien = now.getDate()
+var miesiac = now.getMonth() + 1
+var rok = now.getFullYear()
+
+alert(dzien+'.'+miesiac+'.'+rok)
+
